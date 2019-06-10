6f7932a1df2cfc424dbb70bce8ecde91bf43cc29
mburhanpurkar <maya.burhanpurkar@gmail.com>
2019-06-10 14:03:04 -0400

    also updated for clusters

--- testmod3d.cpp
+++ testmod3d.cpp
@@ -1,17 +1,23 @@
-#include "modxy3d.h"
+////#include "modxy3d.h"
+#include "parallelize.h"
 
-void test_3d() {
-     const int len = 3;
-     int sizes[len] = {19, 20, 21};
-     for (int i=0; i < len; i++) {
-         Metropolis metropolis(sizes[i], "test");
-         metropolis.simulate(1.0, 1e3, 0.1, 2.0, 0.1, 2.0, 0.05);
-     }
+// void test_3d() {
+//      const int len = 3;
+//      int sizes[len] = {19, 20, 21};
+//      for (int i=0; i < len; i++) {
+//          Metropolis metropolis(sizes[i], "test");
+//          metropolis.simulate(1.0, 3e3, 0.1, 13.38, 0.01);
+//      }
+// }
+
+void test_parallel() {
+  mpi_run(0.0, 0.4, 0.7, 64 * 8, 16, 3e3, "test");
 }
 
 int main(int argc, char** argv) {
     // Do unit testing
-    test_3d();
+    // test_3d();
+    test_parallel();
     return 1;
 }
 
