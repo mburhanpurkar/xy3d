d62325da237e06146c1fda692cbb6162dc2cf8a7
mburhanpurkar <maya.burhanpurkar@gmail.com>
2019-06-14 10:28:07 -0400

    slightly better

--- end_the_vorticity_debate.py
+++ end_the_vorticity_debate.py
@@ -30,29 +30,59 @@ def print_for_cpp(arr):
         else:
             print "},",
     print "}"
-            
 
-def make_vortex(charge, sign, start=5*math.pi, verbose=False):
+
+def make_loop(size, start_x, start_y):
+    # Makes a size * size with bottom left corner at (start_x, start_y)
+    x = start_x
+    y = start_y
+
+    ll = [(start_x, start_y)]
+
+    for plus_x in range(0, size - 1):
+        x += 1
+        ll.append((x, y))
+
+    for plus_y in range(0, size - 1):
+        y += 1
+        ll.append((x, y))
+        
+    for minus_x in range(0, size - 1):
+        x -= 1
+        ll.append((x, y))
+        
+    for minus_y in range(0, size - 1):
+        y -= 1
+        ll.append((x, y))
+        
+    return ll
+    
+
+def make_vortex(charge, sign, start=5*math.pi / 4, verbose=False):
     assert(sign == -1 or sign == 1)
     
     # We will construct examples on a 4x4 lattice
-    L = 4
-    outer_loop = [(0, 0), (1, 0), (2, 0), (3, 0), (3, 1), (3, 2), (3, 3), (2, 3), (1, 3), (0, 3), (0, 2), (0, 1), (0, 0)]
-    inner_loop = [(1, 1), (2, 1), (2, 2), (1, 2), (1, 1)]
-
+    L = 6
+    outer_outer_loop = make_loop(6, 0, 0)
+    outer_loop = make_loop(4, 1, 1)
+    inner_loop = make_loop(2, 2, 2)
+    
     # Construct array
-    spins = np.empty((4, 4))
-    spins[outer_loop[0]] = start / L
-    spins[inner_loop[0]] = start / L
+    spins = np.empty((L, L))
+    spins[outer_loop[0]] = start
+    spins[inner_loop[0]] = start
 
     # Outer loop over outer_loop
-    outer_spacing = charge * 2 * math.pi / 12
-    inner_spacing = charge * 2 * math.pi / 4
+    outer_spacing = charge * 2 * math.pi / 12.0
+    inner_spacing = charge * 2 * math.pi / 4.0
     for i in xrange(len(outer_loop[2:])):
         spins[outer_loop[i + 1]] = (spins[outer_loop[i]] + sign  * outer_spacing) % (2 * math.pi)
 
     for i in xrange(len(inner_loop[2:])):
         spins[inner_loop[i + 1]] = (spins[inner_loop[i]] + sign * inner_spacing) % (2 * math.pi)
+
+    for i in xrange(len(outer_outer_loop)):
+        spins[outer_outer_loop[i]] = np.random.rand() * 2 * np.pi
 
     # Try computing the vorticity! For some reason, doing [-pi, pi] seems to work int the C++ implementation
     # (I'll figure this out tonight?) so let's give that a shot:
